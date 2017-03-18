9f6f1d30d72fc2c3210ca42c6c2974f6ee7685e0
Yoshi <yoshijava@gmail.com>
2017-03-19 06:03:21 +0800

    Added pseudo x86 assembly as the rollback one.

--- MacroBase.cc
+++ MacroBase.cc
@@ -1,81 +1,117 @@
 #include <iostream>
 
 using namespace std;
-#define HAVE_ISA
+#define MY_ISA
 #define NUM_VEC_LANES 64
 
 template <typename T>
-struct Var {
+struct Var
+{
     T val;
     const char *name;
-    Var(const char *name, T val) {
+    Var(const char *name, T val)
+    {
         this->name = name;
         this->val = val;
     }
 };
 
 #define DECLARE_I32(x, val) \
-    Var<int> i_##x(#x, val); \
+    Var<int> i_##x(#x, val);
 
 #define ADD_32(x, y, z) \
-    add_32_have_impl(i_##x, i_##y, i_##z); \
+    add_32_my_isa_impl(i_##x, i_##y, i_##z);
 
 #define ADD_32_IMM16(x, y, z) \
-    add_32_imm16_have_impl(i_##x, i_##y, z); \
+    add_32_imm16_my_isa_impl(i_##x, i_##y, z);
 
 #define ADD_SAT32(x, y, z) \
-    add_sat32_have_impl(i_##x, i_##y, i_##z); \
+    add_sat32_my_isa_impl(i_##x, i_##y, i_##z);
 
 #define RSHIFT(x, y, z) \
-    rshift_have_impl(i_##x, i_##y, i_##z); \
+    rshift_my_isa_impl(i_##x, i_##y, i_##z);
 
 #define LSHIFT(x, y, z) \
-    lshift_have_impl(i_##x, i_##y, i_##z); \
+    lshift_my_isa_impl(i_##x, i_##y, i_##z);
 
 #define MUL_32(x, y, z) \
-    mul32_have_impl(i_##x, i_##y, i_##z);
+    mul32_my_isa_impl(i_##x, i_##y, i_##z);
 
-void lshift_have_impl(Var<int> &x, Var<int> y, Var<int> z) {
+void lshift_my_isa_impl(Var<int> &x, Var<int> y, Var<int> z)
+{
     x.val = y.val << z.val;
+#ifdef MY_ISA
     printf("s_lshift.%d %s=0x%08x, %s=0x%08x\n", z.val, x.name, x.val, y.name, y.val);
+#else
+    printf("x86-lshift.%d %s=0x%08x, %s=0x%08x\n", z.val, x.name, x.val, y.name, y.val);
+#endif
 }
 
-void rshift_have_impl(Var<int> &x, Var<int> y, Var<int> z) {
+void rshift_my_isa_impl(Var<int> &x, Var<int> y, Var<int> z)
+{
     x.val = y.val >> z.val;
+#ifdef MY_ISA
     printf("s_rshift.%d %s=0x%08x, %s=0x%08x\n", z.val, x.name, x.val, y.name, y.val);
+#else
+    printf("x86-rshift.%d %s=0x%08x, %s=0x%08x\n", z.val, x.name, x.val, y.name, y.val);
+#endif
 }
 
-void mul32_have_impl(Var<int> &x, Var<int> y, Var<int> z) {
+void mul32_my_isa_impl(Var<int> &x, Var<int> y, Var<int> z)
+{
     x.val = y.val * z.val;
+#ifdef MY_ISA
     printf("s_mul.i_i, %s=0x%08x, %s=0x%08x, %s=0x%08x\n", x.name, x.val, y.name, y.val, z.name, z.val);
+#else
+    printf("x86-mul, %s=0x%08x, %s=0x%08x, %s=0x%08x\n", x.name, x.val, y.name, y.val, z.name, z.val);
+#endif
 }
 
-void add_32_imm16_have_impl(Var<int> &x, Var<int> y, int z) {
+void add_32_imm16_my_isa_impl(Var<int> &x, Var<int> y, int z)
+{
     x.val = y.val + static_cast<short>(z);
+#ifdef MY_ISA
     printf("s_add.i_i, %s=0x%08x, %s=0x%08x, 0x%08x\n", x.name, x.val, y.name, y.val, z);
+#else
+    printf("x86-addi, %s=0x%08x, %s=0x%08x, 0x%08x\n", x.name, x.val, y.name, y.val, z);
+#endif
 }
 
-void add_32_have_impl(Var<int> &x, Var<int> y, Var<int> z) {
+void add_32_my_isa_impl(Var<int> &x, Var<int> y, Var<int> z)
+{
     x.val = y.val + z.val;
+#ifdef MY_ISA
     printf("s_add.i_i, %s=0x%08x, %s=0x%08x, %s=0x%08x\n", x.name, x.val, y.name, y.val, z.name, z.val);
+#else
+    printf("x86-add, %s=0x%08x, %s=0x%08x, %s=0x%08x\n", x.name, x.val, y.name, y.val, z.name, z.val);
+#endif
 }
 
-void add_sat32_have_impl(Var<int> &x, Var<int> y, Var<int> z) {
-    // HAVE SPU implementation, assuming saturation add
+void add_sat32_my_isa_impl(Var<int> &x, Var<int> y, Var<int> z)
+{
+    // my_isa SPU implementation, assuming saturation add
     long long result = static_cast<long long>(y.val) + static_cast<long long>(z.val);
-    if(result > INT_MAX) {
+    if (result > INT_MAX)
+    {
         x.val = INT_MAX;
     }
-    else if(result < INT_MIN) {
+    else if (result < INT_MIN)
+    {
         x.val = INT_MIN;
     }
-    else {
+    else
+    {
         x.val = static_cast<int>(result);
     }
+#ifdef MY_ISA
     printf("s_add_sat.i_i, %s=0x%08x, %s=0x%08x, %s=0x%08x\n", x.name, x.val, y.name, y.val, z.name, z.val);
+#else
+    printf("x86-add-sat, %s=0x%08x, %s=0x%08x, %s=0x%08x\n", x.name, x.val, y.name, y.val, z.name, z.val);
+#endif
 }
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
     DECLARE_I32(result, 0)
     DECLARE_I32(r0, 0x7fffffff)
     DECLARE_I32(r1, 0x1)
