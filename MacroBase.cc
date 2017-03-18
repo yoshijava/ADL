6c06c9cb7724a26dce26f64d48497a3fb62b7235
Yoshi <yoshijava@gmail.com>
2017-03-19 00:05:13 +0800

    Use HAVE_ISA to set to have isa; otherwise x86 instead;

--- MacroBase.cc
+++ MacroBase.cc
@@ -1,23 +1,37 @@
 #include <iostream>
-// #include <string>
 
 using namespace std;
+#define HAVE_ISA true
 
 #define DECLARE_U32(x, val) \
     int i_##x = val;
 
 #define ADD32(x, y, z) \
-    i_##x = i_##y + i_##z; \
-    add32_have_impl(i_##x, i_##y, i_##z); // HAVE
-    
+if(HAVE_ISA) { \
+    add32_have_impl(i_##x, i_##y, i_##z); \
+} \
+else { \
+    add32_x86_impl(i_##x, i_##y, i_##z); \
+} \
 
 void add32_have_impl(int x, int y, int z) {
-    // HAVE SPU implementation
+    // HAVE SPU implementation, assuming saturation add
+    long long result = y + z;
+    if(x > INT_MAX) {
+        x = INT_MAX;
+    }
+    else if(x < INT_MIN) {
+        x = INT_MIN;
+    }
+    else {
+        x = static_cast<int>(result);
+    }
     printf("%s, 0x%08x, 0x%08x, 0x%08x\n", "s_add.i_i", x, y, z);
 }
 
 void add32_x86_impl(int x, int y, int z) {
     // x86 implementation
+    x = y + z;
     printf("%s, 0x%08x, 0x%08x, 0x%08x\n", "ADD", x, y, z);
 }
 
