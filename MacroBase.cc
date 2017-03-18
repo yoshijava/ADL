c380356f8217fca1e83c5db002ea72969220f4c5
Yoshi <yoshijava@gmail.com>
2017-03-18 23:48:52 +0800

    You can change to x86 implementation.

--- MacroBase.cc
+++ MacroBase.cc
@@ -8,10 +8,17 @@ using namespace std;
 
 #define ADD32(x, y, z) \
     i_##x = i_##y + i_##z; \
-    add32_impl(i_##x, i_##y, i_##z);
+    add32_have_impl(i_##x, i_##y, i_##z); // HAVE
+    
 
-void add32_impl(int x, int y, int z) {
+void add32_have_impl(int x, int y, int z) {
+    // HAVE SPU implementation
     printf("%s, 0x%08x, 0x%08x, 0x%08x\n", "s_add.i_i", x, y, z);
+}
+
+void add32_x86_impl(int x, int y, int z) {
+    // x86 implementation
+    printf("%s, 0x%08x, 0x%08x, 0x%08x\n", "ADD", x, y, z);
 }
 
 int main(int argc, char *argv[]) {
