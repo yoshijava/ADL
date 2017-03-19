374b887c1c5579d9d6be7724f7bab45e2b63ac2a
Yoshi <yoshijava@gmail.com>
2017-03-19 19:44:13 +0800

    Operator overloaded. + is a saturation add.

--- MacroBase.cc
+++ MacroBase.cc
@@ -12,14 +12,15 @@ int main(int argc, char *argv[])
     DECLARE_I32(r0, 0x7fffffff)
     DECLARE_I32(r1, 0x1)
 
+    // Normal scalar operations
     ADD_SAT32(result, r0, r1)
     ADD_32_IMM16(result, r1, 0x1)
-
     DECLARE_I32(r2, 4)
     MUL_32(result, r2, r2)
     RSHIFT(result, r2, r1)
     LSHIFT(result, r2, r1)
 
+    // Vector lshift
     DECLARE_I32(r3, 1)
     for(int i=0; i<7; i++) {
         LSHIFT_IMM16(r3, r3, 1)
@@ -27,6 +28,13 @@ int main(int argc, char *argv[])
 
     DECLARE_VEC_I32(v1, 1);
     LSHIFT_VEC(v1, v1, r1)
+
+    // Operator overload. It calls the underlying saturation add implementation
+    // Defined as operator ~
+    result = r0 + r1;
+    cout << "r0 = " << r0 << endl;
+    cout << "r1 = " << r1 << endl;
+    cout << "r0 + r1 = " << result << endl;
 
     return 0;
 }
--- Scalar.h
+++ Scalar.h
@@ -1,23 +1,38 @@
 #ifndef SCALAR_H_
 #define SCALAR_H_
-#define DECLARE_I32(x, val) Var<int> i_##x(#x, val);
-#define ADD_32(x, y, z) add_32_my_isa_impl(i_##x, i_##y, i_##z);
-#define ADD_32_IMM16(x, y, z) add_32_imm16_my_isa_impl(i_##x, i_##y, z);
-#define ADD_SAT32(x, y, z) add_sat32_my_isa_impl(i_##x, i_##y, i_##z);
-#define RSHIFT(x, y, z) rshift_my_isa_impl(i_##x, i_##y, i_##z);
-#define LSHIFT(x, y, z) lshift_my_isa_impl(i_##x, i_##y, i_##z);
-#define LSHIFT_IMM16(x, y, z) lshift_imm16_my_isa_impl(i_##x, i_##y, z);
-#define MUL_32(x, y, z) mul32_my_isa_impl(i_##x, i_##y, i_##z);
+#include <iostream>
+
+#define DECLARE_I32(x, val) Var<int> x(#x, val);
+#define ADD_32(x, y, z) add_32_my_isa_impl(x, y, z);
+#define ADD_32_IMM16(x, y, z) add_32_imm16_my_isa_impl(x, y, z);
+#define ADD_SAT32(x, y, z) add_sat32_my_isa_impl(x, y, z);
+#define RSHIFT(x, y, z) rshift_my_isa_impl(x, y, z);
+#define LSHIFT(x, y, z) lshift_my_isa_impl(x, y, z);
+#define LSHIFT_IMM16(x, y, z) lshift_imm16_my_isa_impl(x, y, z);
+#define MUL_32(x, y, z) mul32_my_isa_impl(x, y, z);
 
 template <typename T>
-struct Var
+class Var
 {
+public:
     T val;
     const char *name;
     Var(const char *name, T val)
     {
         this->name = name;
         this->val = val;
+    }
+    // this add function is defined as saturation add
+    inline friend Var<T> operator+(Var<T> &lhs, const Var<T> &rhs)
+    {
+        Var<T> result("result", 0);
+        add_sat32_my_isa_impl(result, lhs, rhs);
+        return result;
+    }
+    inline friend std::ostream& operator<<(std::ostream& os, const Var<T> &obj)
+    {
+        os << obj.val;
+        return os;
     }
 };
 
